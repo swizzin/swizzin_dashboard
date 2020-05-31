bd5618cdde3b84c21093f75ee3138e6a674eae06
liaralabs <30909703+liaralabs@users.noreply.github.com>
2020-05-30 17:55:58 -0700

    Fixes for vnstat and login realm (#12)
    * Specify flask login realm
    
    * Fix hour/lasthour function
    
    * fix index exception

--- core/config.py
+++ core/config.py
@@ -5,6 +5,7 @@ class Config:
     APPLICATION_ROOT = '/'
     FLASK_HTPASSWD_PATH = '/etc/htpasswd'
     FLASK_SECRET = "What's the password?"
+    FLASK_AUTH_REALM = "What's the password?"
     HOST = "0.0.0.0"
     PORT = "8333"
     URL_BASE = "/"
