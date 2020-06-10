78c5f41f66d465a1e4d077808838a3e443c5c53d
liaralabs <liara@swizzin.ltd>
2020-06-10 16:56:30 -0700

    Fix vnstat hourly reporting under Focal

--- core/util.py
+++ core/util.py
@@ -203,7 +203,12 @@ def vnstat_data(interface, mode):
 def vnstat_parse(interface, mode, query, position=False):
     if position is not False:
         #result = vnstat_data(interface, mode)['interfaces'][0]['traffic'][query][position]
-        result = vnstat_data(interface, mode)['interfaces'][0]['traffic'][query]
+        if query == "hours"
+            try:
+                result = vnstat_data(interface, mode)['interfaces'][0]['traffic'][query]
+            except:
+                query = "hour"
+                result = vnstat_data(interface, mode)['interfaces'][0]['traffic'][query]
         for p in result:
             if p["id"] == int(position):
                 data = {}
